115c1bacabeeddb1d141fb86536f5428dcb58dd2
Cole Conley <cole.conley2018@gmail.com>
2025-05-28 17:35:46 -0400

    what

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const box = 20;
+const canvasSize = 20;
+canvas.width = box * canvasSize;
+canvas.height = box * canvasSize;
+
 let snake = [{ x: 9 * box, y: 10 * box }];
 let food = {
-    x: Math.floor(Math.random() * 19 + 1) * box,
-    y: Math.floor(Math.random() * 19 + 1) * box,
+    x: Math.floor(Math.random() * canvasSize) * box,
+    y: Math.floor(Math.random() * canvasSize) * box,
 };
 
 let direction = "RIGHT";
@@ -16,22 +20,39 @@ document.addEventListener("keydown", changeDirection);
 
 function changeDirection(event) {
     const key = event.keyCode;
+    console.log("Key pressed: ", key);
     if (key == 37 && direction !== "RIGHT") direction = "LEFT";
     else if (key == 38 && direction !== "DOWN") direction = "UP";
     else if (key == 39 && direction !== "LEFT") direction = "RIGHT";
     else if (key == 40 && direction !== "UP") direction = "DOWN";
 }
 
+function drawGrid() {
+    ctx.strokeStyle = "#333";
+    for (let i = 0; i < canvasSize; i++) {
+        for (let j = 0; j < canvasSize; j++) {
+            ctx.strokeRect(i * box, j * box, box, box);
+        }
+    }
+}
+
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawGrid();
 
     for (let i = 0; i < snake.length; i++) {
         ctx.fillStyle = i == 0 ? "green" : "lime";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "darkgreen";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
 
     ctx.fillStyle = "red";
     ctx.fillRect(food.x, food.y, box, box);
+    ctx.strokeStyle = "darkred";
+    ctx.strokeRect(food.x, food.y, box, box);
 
     let headX = snake[0].x;
     let headY = snake[0].y;
@@ -44,8 +65,8 @@ function draw() {
     if (headX == food.x && headY == food.y) {
         score++;
         food = {
-            x: Math.floor(Math.random() * 19 + 1) * box,
-            y: Math.floor(Math.random() * 19 + 1) * box,
+            x: Math.floor(Math.random() * canvasSize) * box,
+            y: Math.floor(Math.random() * canvasSize) * box,
         };
     } else {
         snake.pop();
@@ -69,7 +90,7 @@ function draw() {
 
     ctx.fillStyle = "white";
     ctx.font = "20px Arial";
-    ctx.fillText("Score: " + score, 10, 390);
+    ctx.fillText("Score: " + score, 10, canvas.height - 10);
 }
 
 function collision(head, array) {
